9f88843306c3fe4cb648d288f517e5a26c13c330
Obed-Ojingwa <obedojingwa@gmail.com>
2026-05-18 20:22:25 +0100

    UI adjustment

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <>
       <Navbar />
-      <main className="min-h-[calc(100vh-rem_calc(3.5rem+rem_calc(3.5rem)))] pb-[rem_calc(3.5rem)]">
+      <main className="min-h-[calc(100vh-72px)] pb-[3.5rem]">
         <slot />
       </main>
       <Footer />
